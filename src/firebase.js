638774053be8747e05723a88b6932cb6d6494f26
SukumizuYoujo <kuroazuki0227@gmail.com>
2025-09-04 03:49:23 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,19 +4,20 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPjTnVSjSurZh-C7CfbhFEOHw6XUen728",
-  authDomain: "mytask-4b2a5.firebaseapp.com",
-  databaseURL: "https://mytask-4b2a5-default-rtdb.firebaseio.com",
-  projectId: "mytask-4b2a5",
-  storageBucket: "mytask-4b2a5.firebasestorage.app",
-  messagingSenderId: "544411020801",
-  appId: "1:544411020801:web:a76d4d925865dedebf1a00",
-  measurementId: "G-LE0B6P60L3"
+  apiKey: "__VITE_API_KEY__",
+  authDomain: "__VITE_AUTH_DOMAIN__",
+  databaseURL: "__VITE_DATABASE_URL__",
+  projectId: "__VITE_PROJECT_ID__",
+  storageBucket: "__VITE_STORAGE_BUCKET__",
+  messagingSenderId: "__VITE_MESSAGING_SENDER_ID__",
+  appId: "__VITE_APP_ID__",
+  measurementId: "__VITE_MEASUREMENT_ID__"
 };
 
 const app  = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db   = getFirestore(app)
+
 
 
 
