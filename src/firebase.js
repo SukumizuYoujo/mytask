d5e6da86af4f42c927ee09b3b3422932e92ff35c
SukumizuYoujo <kuroazuki0227@gmail.com>
2025-09-04 03:14:48 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,15 +5,18 @@ import { getAuth }        from 'firebase/auth'
 import { getFirestore }   from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey:            process.FIREBASE_API_KEY,
-  authDomain:        process.FIREBASE_AUTH_DOMAIN,
-  projectId:         process.FIREBASE_PROJECT_ID,
-  storageBucket:     process.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.FIREBASE_MESSAGING_SENDER_ID,
-  appId:             process.FIREBASE_APP_ID
-}
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+};
 
 const app  = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db   = getFirestore(app)
+
 
