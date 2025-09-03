7398f8151526d94c60428f9074a3ce91a316e62a
SukumizuYoujo <kuroazuki0227@gmail.com>
2025-09-04 03:30:09 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
 // src/firebase.js
-import 'dotenv/config'
 import { initializeApp } from 'firebase/app'
 import { getAuth }        from 'firebase/auth'
 import { getFirestore }   from 'firebase/firestore'
@@ -18,5 +17,6 @@ const firebaseConfig = {
 const app  = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db   = getFirestore(app)
+
 
 
