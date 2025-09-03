6931801e28e09734462ff22817d90af580ff8a58
SukumizuYoujo <kuroazuki0227@gmail.com>
2025-09-04 03:35:54 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // src/firebase.js
-import { initializeApp } from 'firebase/app'
-import { getAuth }        from 'firebase/auth'
-import { getFirestore }   from 'firebase/firestore'
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -17,6 +17,7 @@ const firebaseConfig = {
 const app  = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db   = getFirestore(app)
+
 
 
 
